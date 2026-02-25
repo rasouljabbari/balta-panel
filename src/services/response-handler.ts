65480d47346efc410439774cc9840f01ef266210
r.jabbari <r.jabbari@rahkargostaran.ir>
2026-02-25 12:27:07 +0330

    fix 401 error

--- src/services/response-handler.ts
+++ src/services/response-handler.ts
@@ -1,18 +1,9 @@
-import { deleteCookie } from "../utils/cookies";
 
 export const handleResponse = (response: any): any => {
   const status = response?.status;
-  console.log("response", response)
 
   if ([200, 201, 204].includes(status)) {
     return response.data;
   }
-
-  if (status === 401) {
-    deleteCookie('auth_token');
-    window.location.href = '/login';
-    return;
-  }
-
   throw response;
 };
